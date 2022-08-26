50936e3af27b0abbcf0f2d306fb158c3d2f27b20
George Navin Fernando <41060349+gnfa10@users.noreply.github.com>
2022-08-26 14:21:17 +0200

    Create play.hpp
    header file with all function declarations

---
+++ play.hpp
@@ -0,0 +1,11 @@
+// function declarations header file
+
+void introduction(); 
+void take_turn(); 
+bool is_winner(); // checks for winner
+bool is_filled(); // continously check if board is filled
+void set_position(); //sets the input from user in the right location in array based on i/p
+void update_board(); //checks if input is from user 1 or 2 and updates board with 'X' or 'O'
+void change_player(); // iterate player turn
+void draw(); // output board with user i/p 
+void end_game();
